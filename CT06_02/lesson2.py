9e9dc92bd0ada6810cceaf8d173edbb585023068
LuLuCheeky <lulucheekypro@gmail.com>
2025-01-19 09:25:22 +0000

    Jan 19, 2025, 9:25 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -6,9 +6,37 @@ print("Hello from lesson 2")
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
 
-Age = (11)
-print(Age)
+# Age = (11)
+# print(Age)
 
-x = 10
-x = 20
-print(x)
+# x = 10
+# x = 20
+# print(x)
+
+
+# Program to check if a number is prime or not
+
+num = 29
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+if num == 0 or num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+    # check if flag is True
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
