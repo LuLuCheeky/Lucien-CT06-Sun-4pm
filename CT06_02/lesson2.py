379ca925fb10d018080c0adfc71ec59085819494
LuLuCheeky <lulucheekypro@gmail.com>
2025-01-19 08:18:39 +0000

    Jan 19, 2025, 8:18 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -5,11 +5,11 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
-print("red")
-print("orange")
+# print("red")
+# print("orange")
 print("yellow")
-print("green")
-print("blue")
-print("indigo")
-print("violet")
+# print("green")
+# print("blue")
+# print("indigo")
+# print("violet")
 
