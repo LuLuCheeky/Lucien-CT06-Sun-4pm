9d39cc20c50b476beeb8cb80bc8c4ceb047e2e13
LuLuCheeky <lulucheekypro@gmail.com>
2025-01-19 09:22:31 +0000

    Jan 19, 2025, 9:22 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -5,19 +5,6 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
-# print("red")
-# print("orange")
-print("yellow")
-# print("green")
-# print("blue")
-# print("indigo")
-# print("violet")
 
-# test_1 = ask input "What is your score of test 1?"
-# test_2 = ask input "What is your score of test 2?"
-# test_3 = ask input "What is your score of test 3?"
-# test_1 * 20% = weightage_of_test_1
-# test_2 * 40% = weightage_of_test_2
-# test_3 * 40% = weightage_of_test_3
-# total_weightage = weightage_of_test_1 + weightage_of_test_2 + weightage_of_test_3
-# print total_weightage
+
+
