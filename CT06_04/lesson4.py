be86ee93b7dda5c38970a8c26ecc24173357bfee
LuLuCheeky <lulucheekypro@gmail.com>
2025-01-26 09:36:54 +0000

    Jan 26, 2025, 9:36 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,22 +1,9 @@
-# Red = 1
-# blue = 2
-# green = 3
-# Name = input("What is your name? ")
-# NumRed = int(input("How many red plates? "))
-# NumBlue = int(input("How many red plates? "))
-# NumGreen = int(input("How many red plates? "))
-# total = (Red * NumRed) + (blue * NumBlue) + (green * NumGreen)
-# print(Name + ", you owe us $" + str(total))
-
-
-def crash():
-        '''\
-        crash the Python interpreter...
-        '''
-        i = ctypes.c_char('a')
-        j = ctypes.pointer(i)
-        c = 0
-        while True:
-                j[c] = 'a'
-                c += 1
-        j
+Red = 1
+blue = 2
+green = 3
+Name = input("What is your name? ")
+NumRed = int(input("How many red plates? "))
+NumBlue = int(input("How many red plates? "))
+NumGreen = int(input("How many red plates? "))
+total = (Red * NumRed) + (blue * NumBlue) + (green * NumGreen)
+print(Name + ", you owe us $" + str(total))
