433da10f6b94f58ebbfdcf45b0ad1552ae3f9bd8
LuLuCheeky <lulucheekypro@gmail.com>
2025-01-26 09:23:13 +0000

    Jan 26, 2025, 9:23 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,7 +1,7 @@
 Red = 1
 blue = 2
 green = 3
-Name = input("What is your name?")
+Name = input("What is your name? ")
 NumRed = int(input("How many red plates? "))
 NumBlue = int(input("How many red plates? "))
 NumGreen = int(input("How many red plates? "))
