13a1c71d93f49fe66d5fb5673c4abca2a0a89a2f
LuLuCheeky <lulucheekypro@gmail.com>
2025-01-26 09:13:42 +0000

    Jan 26, 2025, 9:13 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,8 +1,8 @@
 Red = 1
 blue = 2
 green = 3
-NumRed = int(input("How many red plates"))
-NumBlue = int(input("How many red plates"))
-NumGreen = int(input("How many red plates"))
+NumRed = int(input("How many red plates? "))
+NumBlue = int(input("How many red plates? "))
+NumGreen = int(input("How many red plates? "))
 total = (Red * NumRed) + (blue * NumBlue) + (green * NumGreen)
 print (total)
