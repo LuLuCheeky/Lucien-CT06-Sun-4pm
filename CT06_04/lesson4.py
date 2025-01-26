f113042fc4ce0a3e4b8bd4e0d81ff2b5b4257ba8
LuLuCheeky <lulucheekypro@gmail.com>
2025-01-26 08:51:45 +0000

    Jan 26, 2025, 8:51 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -6,5 +6,3 @@ green = 3
 total = (3 * red) + (5 * blue) + (4 * green)
 print("You have spent a total of $" + str(total))
 
-name = input('What is your name?')     # \n ---> newline  ---> It causes a line break
-print(name)
