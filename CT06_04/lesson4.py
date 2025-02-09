4a29551c7874cbab1852d3b42d3ed8ba1daad2e6
LuLuCheeky <lulucheekypro@gmail.com>
2025-02-09 08:02:25 +0000

    Feb 9, 2025, 8:02 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,12 +1,12 @@
-# Red = 1
-# blue = 2
-# green = 3
-# Name = input("What is your name? ")
-# NumRed = int(input("How many red plates? "))
-# NumBlue = int(input("How many red plates? "))
-# NumGreen = int(input("How many red plates? "))
-# total = (Red * NumRed) + (blue * NumBlue) + (green * NumGreen)
-# print(Name + ", you owe us $" + str(total))
+Red = 1
+blue = 2
+green = 3
+Name = input("What is your name? ")
+NumRed = int(input("How many red plates? "))
+NumBlue = int(input("How many red plates? "))
+NumGreen = int(input("How many red plates? "))
+total = (Red * NumRed) + (blue * NumBlue) + (green * NumGreen)
+print(Name + ", you owe us $" + str(total))
 
 for i in range (1, 10):
     print("HELPPP")
