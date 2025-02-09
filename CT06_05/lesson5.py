e7773ae24fd49b2d2b45117219c3cdcd65925067
LuLuCheeky <lulucheekypro@gmail.com>
2025-02-09 08:15:42 +0000

    Feb 9, 2025, 8:15 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,2 +1,2 @@
-age = input('How old are you this year? ')
-print('You were born in' ,  2025 - int(age) , '.')
+# age = input('How old are you this year? ')
+# print('You were born in' ,  2025 - int(age) , '.')
