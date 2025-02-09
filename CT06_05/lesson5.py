9ee2b7b676ebb64940168d381571d3e8d91a55d4
LuLuCheeky <lulucheekypro@gmail.com>
2025-02-09 08:29:07 +0000

    Feb 9, 2025, 8:29 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -2,7 +2,8 @@
 # print('You were born in' ,  2025 - int(age) , '.')
 
 Name = input("Whats the participant's name? ")
-Age = input("What year was the participant born in? ")
+Age = int(input("What year was the participant born in? "))
 Message = input("Give the participant a personalised massage: ")
+
 
 print("Happy" , Age , "th brithday" , Name , "!" , Message )
