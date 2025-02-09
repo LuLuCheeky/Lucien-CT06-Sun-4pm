2d6555364fc88d76de345e2cc64b4c2ddabbec56
LuLuCheeky <lulucheekypro@gmail.com>
2025-02-09 08:18:19 +0000

    Feb 9, 2025, 8:18 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,6 +1,8 @@
 # age = input('How old are you this year? ')
 # print('You were born in' ,  2025 - int(age) , '.')
 
-Name = input("Whats the participant's name?")
-Age = input("How old is the participant?")
-Mesage = input("Give the participant a personalised massage:")
+Name = input("Whats the participant's name? ")
+Age = input("How old is the participant? ")
+Mesage = input("Give the participant a personalised massage: ")
+
+print
