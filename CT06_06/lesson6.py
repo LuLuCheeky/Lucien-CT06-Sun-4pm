84ea76ac84ececac0e78ac0dd84937e7d52efe11
LuLuCheeky <lulucheekypro@gmail.com>
2025-02-16 08:25:01 +0000

    Feb 16, 2025, 8:25 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -5,3 +5,4 @@ totalMarks = 0
 for i in numStu:
     #Average = the sum of ALL the numbers divided by num of numbers
     totalMarks = totalMarks + int(input("How much did student " + numStu - i + " get? " ))
+print()
