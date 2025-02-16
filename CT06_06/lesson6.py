b3536a997cd7148e6eb67fe57bf95ae65287f365
LuLuCheeky <lulucheekypro@gmail.com>
2025-02-16 08:34:36 +0000

    Feb 16, 2025, 8:34 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -4,5 +4,5 @@ numStu = int(input("How many students in the class are there? "))
 totalMarks = 0
 for i in range(numStu):
     #Average = the sum of ALL the numbers divided by num of numbers
-    totalMarks = totalMarks + int(input("How much did student number " + numStu - i + " get? " ))
+    totalMarks = totalMarks + int(input("How much did student number " + str(numStu) - i + " get? " ))
 print("The average score of all the students were " + str(totalMarks / numStu))
