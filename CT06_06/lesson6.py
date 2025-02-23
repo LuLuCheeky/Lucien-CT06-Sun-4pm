56a2046584b65db77d9abeb1a4b3cce943f9fefd
LuLuCheeky <lulucheekypro@gmail.com>
2025-02-23 07:49:16 +0000

    Feb 23, 2025, 7:49 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,9 +1,9 @@
 # # print("Hello from lesson 6")
 
-# numStu = int(input("How many students in the class are there? "))
-# totalMarks = 0
-# stu = 1
-for i in range(numStu):
+numStu = int(input("How many students in the class are there? "))
+totalMarks = 0
+stu = 1
+for i in range(numStu):  
     #Average = the sum of ALL the numbers divided by num of numbers
     totalMarks = totalMarks + int(input("How much did student number " + str(stu) + " get? " ))
     stu = stu + 1
