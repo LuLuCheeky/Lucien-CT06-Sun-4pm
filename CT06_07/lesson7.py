165f02040e19efac7cbb68dc48b4063702ccb21d
LuLuCheeky <lulucheekypro@gmail.com>
2025-02-23 09:01:13 +0000

    Feb 23, 2025, 9:01 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,5 +1,5 @@
 total = 0
 for i in range(1, 6):
-    total = total + int(input("What is number #" + str(i) , "? "))
+    total = total + int(input("What is number #" + str(i) + " ? "))
 
 print(total)
