f6b8b48efe26950dfd4c9379235573dfd3100261
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-01 13:08:03 +0000

    Mar 1, 2025, 1:08 PM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -4,4 +4,4 @@
 #    print(str(num) + " x " + str(i) + " = " + str(num2))
 
 dist = input("How much is the distance to the place you are driving to? ")
-print("You should drive at the average speed of" , )
+print("You should drive at the average speed of" , str())
