1490a889c2702b4826ba6f692d6a0d3ceaf74679
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-02 08:25:37 +0000

    Mar 2, 2025, 8:25 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,8 +1,7 @@
 total = 1
 num = 1
-num2 = 0
 for i in range(1, 6):
-    total = total * num2 = int(input("Whats the" , num , "number? "))
+    total = total * num2 = int(input("Whats the" , str(i) , "number? "))
     num = num + 1
 print(total)
 
