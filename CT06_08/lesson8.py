cc728f2ae78fd51d8404069c03da35a627c2c1c4
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-02 08:04:20 +0000

    Mar 2, 2025, 8:04 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,3 +1,4 @@
 for i in range(1, 6):
     1 = input("Whats the first number? ")
-    2 = input("Whats the first number? ")
+    2 = input("Whats the second number? ")
+    3 = input("Whats the first number? ")
