f12cd40437e3ff5f421e878cd45c0aeff28b9b68
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-02 08:18:18 +0000

    Mar 2, 2025, 8:18 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,8 +1,8 @@
-for i in range(1, 6):
-    a = int(input("Whats the first number? "))
-    b = int(input("Whats the second number? "))
-    c = int(input("Whats the third number? "))
-    d = int(input("Whats the fourth number? "))
+
+a = int(input("Whats the first number? "))
+b = int(input("Whats the second number? "))
+c = int(input("Whats the third number? "))
+d = int(input("Whats the fourth number? "))
     e = int(input("Whats the fifth number? "))
 print (str( a * b * c * d * e))
 
