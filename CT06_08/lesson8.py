1aef1732a33e324b2457c0af7c50a0fd9372f3f5
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-02 08:18:42 +0000

    Mar 2, 2025, 8:18 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,9 +1,4 @@
-a = int(input("Whats the first number? "))
-b = int(input("Whats the second number? "))
-c = int(input("Whats the third number? "))
-d = int(input("Whats the fourth number? "))
-e = int(input("Whats the fifth number? "))
-print (str( a * b * c * d * e))
+total = 0 
 
 
 
