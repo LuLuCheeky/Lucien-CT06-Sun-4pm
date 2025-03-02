cdb040d23e82806fc68f73d34335f772bf614166
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-02 08:13:20 +0000

    Mar 2, 2025, 8:13 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,7 +1,7 @@
 for i in range(1, 6):
-    1 = int(input("Whats the first number? "))
+    a = int(input("Whats the first number? "))
     2 = int(input("Whats the second number? "))
     3 = int(input("Whats the third number? "))
-    4 = int(input("Whats the fourth number? "))
+    d = int(input("Whats the fourth number? "))
     e = int(input("Whats the fifth number? "))
 print (str())
