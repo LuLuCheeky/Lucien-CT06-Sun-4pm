cd755a06c5b6e2543e2995ddb490bb8e638464c3
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-02 08:14:58 +0000

    Mar 2, 2025, 8:14 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -4,4 +4,4 @@ for i in range(1, 6):
     c = int(input("Whats the third number? "))
     d = int(input("Whats the fourth number? "))
     e = int(input("Whats the fifth number? "))
-print (str( a * b))
+print (str( a * b ))
