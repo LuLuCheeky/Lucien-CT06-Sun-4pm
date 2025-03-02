f7cf68b3051167d40523fbd722c2f403ffaf279b
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-02 09:06:04 +0000

    Mar 2, 2025, 9:06 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,12 +1,14 @@
-#task (1)
-import time
-for i in range(10, 0, -1):
-    print(i)
-    time.sleep(1)
+# #task (1)
+# import time
+# for i in range(10, 0, -1):
+#     print(i)
+#     time.sleep(1)
 
-#task (2)
-import time
-num = int(input("What is the seconds you want to countdown form? "))
-for i in range(num, 0 , -1):
-    print(i)
-    time.sleep(1)
+# #task (2)
+# import time
+# num = int(input("What is the seconds you want to countdown form? "))
+# for i in range(num, 0 , -1):
+#     print(i)
+#     time.sleep(1)
+
+
