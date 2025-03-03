752581fe0971ece116e593554732fdaa2d754b3f
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-03 11:52:48 +0000

    Mar 3, 2025, 11:52 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -47,7 +47,7 @@ while not Try == 1:
     print("Rolling...")
     Try = random.randint(1, num)
     if not Try == 1:
-        print("You got" ,  )
+        print("You got" , )
         time.sleep(1)
     num = num + 2
 
