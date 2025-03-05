de29e1b47da2ed24e0ae1471af106ad9ac5d63ce
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-05 11:44:07 +0000

    Mar 5, 2025, 11:44 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -23,45 +23,31 @@
 import random
 import time
 
-worthlessN = "Worthless"
-worthless = 2
-trashN = "Trash"
-trash = 4
-commonN = "Common"
-common = 8
-uncommonN = "Uncommon"
-uncommon = 16
-rareN = "Rare"
-rare = 32
-epicN = "Epic"
-epic = 64
-uniqueN = "Uncommon"
-unique = 128
-gemstoneN = "Gemstone"
-gemstone = 256
-transcendentN = "Transcendent"
-transcendent = 512
-legendaryN = "Legendary"
-legendary = 1024
-mythicN = "Mythic"
-mythic = 2048
-exoticN = "Exotic"
-exotic = 4096
-divineN = "Divine"
-divine = 8192
-blessedN = "Blessed"
-blessed = 16384
-arcaneN = "Arcane"
-arcane = 32768
-
+Raritys = [
+    "Worthless",
+    "Trash",
+    "Common",
+    "Uncommon",
+    "Rare",
+    "Epic",
+    "Unique",
+    "Gemstone",
+    "Transcendent",
+    "Legendary",
+    "Mythical",
+    "Exotic", 
+    "Divine",
+    "Heavenly"
+    "Arcane"
+]
 
 num = 2
 Rarity = 2
 Try = 0
-Check = 2
+RarityLoop = 2
 while not Try == 1:
     while not Rarity == num:
-        l
+        
     print("Rolling...")
     Try = random.randint(1, num)
     if not Try == 1:
