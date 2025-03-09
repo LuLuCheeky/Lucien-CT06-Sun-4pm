e30615b600ba72b142d1a31a211fd829c01ad0ad
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-09 08:35:09 +0000

    Mar 9, 2025, 8:35 AM

--- test.py
+++ test.py
@@ -4,5 +4,4 @@
 Start = int(input("What number do you want to start at? "))
 End = int(input("What number do you want to end at? "))
 Increment = int(input("What number do you want the increment to be? "))
-for i in range(Start, End + 1, Increment):
-    print(i)
+print(Start , End, Increment)
