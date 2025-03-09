cdb089251ca72894c18333239636db8017f0b741
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-09 08:46:46 +0000

    Mar 9, 2025, 8:46 AM

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ End = int(input("WHat number do you want to end at? "))
 Increment = int(input("What do you want the increment to be? "))
 Print = ""
 for i in range(Start, End, Increment):
-    Print = Print + "," + str(i)
+    Print = Print + ", " + str(i)
 print(Print)
