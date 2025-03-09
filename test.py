70e5c3821be9a8ba81ce8da31990c986f988eae6
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-09 08:56:39 +0000

    Mar 9, 2025, 8:56 AM

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 # name = input("Whats your name? ")
 # print("Nice to meet you," , name + "!")
 
-Start = int(input("What number do you want to start at? "))
-End = int(input("WHat number do you want to end at? "))
-Increment = int(input("What do you want the increment to be? "))
-Print = ""
-for i in range(Start, End, Increment):
-    Print = Print + str(i) + ", "
-print(Print)
+# Start = int(input("What number do you want to start at? "))
+# End = int(input("WHat number do you want to end at? "))
+# Increment = int(input("What do you want the increment to be? "))
+# Print = ""
+# for i in range(Start, End, Increment):
+#     Print = Print + str(i) + ", "
+# print(Print)
