d313f319171ca2c0767e5134d69f45aacfd1a97e
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-23 08:35:26 +0000

    Mar 23, 2025, 8:35 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,9 +1,9 @@
-num = int(input("What number? "))
-num = num > 0
-if num:
-    print("The number you have inputed is positive.")
-else:
-    print("The number you have inputed is negative.")
+# num = int(input("What number? "))
+# num = num > 0
+# if num:
+#     print("The number you have inputed is positive.")
+# else:
+#     print("The number you have inputed is negative.")
 
 import random
 
