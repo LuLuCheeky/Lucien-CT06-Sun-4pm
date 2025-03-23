ef8c3e0e26ffacccb4be3bd10fad59b09423515e
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-23 08:33:35 +0000

    Mar 23, 2025, 8:33 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -4,3 +4,14 @@ if num:
     print("The number you have inputed is positive.")
 else:
     print("The number you have inputed is negative.")
+
+hardness = int(input("What do you want the max number be? "))
+num = str(random.randint(1, hardness))
+guess = 0
+while guess != num:
+    guess = input("What do you think the random number (1 - " + str(hardness) + ") is? ")
+    if not guess == num:
+        print("Wrong! 😡")
+    else:
+        break
+print("Congrats! 😎")
