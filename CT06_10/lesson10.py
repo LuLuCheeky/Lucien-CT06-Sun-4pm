8c06bd077af5add93a99cf04a6407353e2c42215
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-23 08:36:29 +0000

    Mar 23, 2025, 8:36 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -11,6 +11,6 @@ num = str(random.randint(1, 10))
 guess = input("Guess a number from 1 to 10. ")
 if not guess == num:
     print("Wrong! 😡")
-    print("The answer was" , )
+    print("The answer was" , num , ".")
 else:
     print("Congrats! 😎")
