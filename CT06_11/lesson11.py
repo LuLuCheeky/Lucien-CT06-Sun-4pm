6c33fd8d19132444a835ea18c6cb941b99fb2dfd
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-30 08:28:21 +0000

    Mar 30, 2025, 8:28 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,10 +1,11 @@
-thing = input("What is the item you are buying? ")
-px = int(input("How much is the " + thing + "? "))
-if px <= 5:
-    print("Sure buy it why not???????????? ITS SO CHEAPPPP")
-elif px <= 50:
-    print("Are you really sure you want the" , thing + ".")
-elif px <= 500:
-    print("Where are you even getting the money from?? Your slary is only 125 or something a year!")
-elif px > 500:
-    print("Don't buy it unless you have money from the shark loaners! (Why do you even loan money from the sharks in the first place you cooked lol.)")
+# thing = input("What is the item you are buying? ")
+# px = int(input("How much is the " + thing + "? "))
+# if px <= 5:
+#     print("Sure buy it why not???????????? ITS SO CHEAPPPP")
+# elif px <= 50:
+#     print("Are you really sure you want the" , thing + ".")
+# elif px <= 500:
+#     print("Where are you even getting the money from?? Your slary is only 125 or something a year!")
+# elif px > 500:
+#     print("Don't buy it unless you have money from the shark loaners! (Why do you even loan money from the sharks in the first place you cooked lol.)")
+
