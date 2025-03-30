eb9b6928950571d91508d2529c5dc3181b9d8e44
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-30 09:04:45 +0000

    Mar 30, 2025, 9:04 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -38,4 +38,4 @@
 
 Burger = input("Would you like a burger (Answer in True or False) ")
 Drink = input("Would you like a drink? (Answer in True or False) ")
-Fries = input("Would you like fries")
+Fries = input("Would you like fries (Answer in True or False) ")
