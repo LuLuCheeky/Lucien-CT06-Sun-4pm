e252c5068717c7d648915540da65dda358103c8e
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-30 08:19:11 +0000

    Mar 30, 2025, 8:19 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -6,5 +6,5 @@ elif px <= 50:
     print("Are you really sure you want the" , thing + ".")
 elif px <= 500:
     print("Where are you even getting the money from?? Your slary is only 125 or something a year!")
-elif px > 501:
+elif px > 500:
     print("Don't buy it unless you have money from the shark loaners! (Why do you even loan money from the sharks in the first place you cooked lol.)")
