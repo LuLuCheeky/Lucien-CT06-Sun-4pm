143a45e5a823ba54e31a3cfa688f731f14e241f1
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-30 08:54:04 +0000

    Mar 30, 2025, 8:54 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -39,3 +39,5 @@
 day = input("WhAt DaY iS iT? ")
 if not day == "saturday" or day == "Saturday" or day == "sunday" or day == "Sunday":
     print("ItS NoT tHe WeEkEnD yEt NoOb")
+else:
+    print("ItS nOt ")
