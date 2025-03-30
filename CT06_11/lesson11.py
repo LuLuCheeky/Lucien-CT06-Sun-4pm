abd3f403d7c3eba2de4a9738adef50a443214e8d
LuLuCheeky <lulucheekypro@gmail.com>
2025-03-30 08:13:10 +0000

    Mar 30, 2025, 8:13 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,4 +1,6 @@
 thing = input("What is the item you are buying? ")
 px = int(input("How much is" , thing + "? "))
 if px <= 5:
-    print("Sure buy it why not????????????")
+    print("Sure buy it why not???????????? ITS SO CHEAPPPP")
+elif px <= 50:
+    print()
