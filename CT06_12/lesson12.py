14107db0afc56a80c12797359ad5c1cb838b822e
LuLuCheeky <lulucheekypro@gmail.com>
2025-04-06 08:40:17 +0000

    Apr 6, 2025, 8:40 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,1 +1,71 @@
-print("Hello from lesson 12")
+# Lesson 12 - While Loops
+
+# Recap 1: Multiples of 3 and 5
+# Create a program to check if a number is both divisible by 3
+# and 5.
+
+# 1. Ask the user to input a number
+# 2. Using the '%' operator, check if the number is both a
+#    multiple of 3 and 5:
+#     If true, print "The number is divisible by 3 and 5!"
+# 3. Else, print "The number is not divisible by 3 and 5"
+
+
+# NUM = int(input("What number? "))
+# if NUM % 3 == 0 and NUM % 5 == 0:
+#     print("The number is divisable by three and five.")
+# else:
+#     print("The number is not divisable by three and five.")
+
+# Task 1: Introduction to while loop
+# **Task 1a**:
+# Due to a pandemic, the government placed a limit to the number
+# of visitors a venue can have.
+
+# Using a 'while' loop, create a program that will increase the
+# number of visitors by 1 before printing out the number of
+# visitors admitted, until number of visitors reaches 50.
+
+# 1. Create a 'visitors' variable and assign '0' to it
+# 2. While there is less than 50 visitors,
+#     I. Increase the visitor count by 1
+#     II. Print the visitor count
+
+# (For Task 1b & 1c)
+# Modify your program to account for the number of visitors
+# already present at the venue, and the number of maximum visitors
+# allowed for the following:
+
+# **Task 1b**:
+# Visitors already present: 18
+# Max visitors allowed: 30
+
+# **Task 1c**:
+# Visitors already present: 4
+# Max visitors allowed: 25
+ 
+# visitors = 0
+
+# while visitors < 50:
+#     visitors = visitors + 1
+#     print("Visitors:" + str(visitors))
+
+# visitors = 18
+
+# while visitors < 30:
+#     visitors = visitors + 1
+#     print("Visitors:" + str(visitors))
+
+
+# visitors = 4
+
+# while visitors < 25:
+#     visitors = visitors + 1
+#     print("Visitors:" + str(visitors))
+
+visitors = 0
+while True:
+    if visitors == 30:
+        break
+    else:
+        visitors = visitors + 1
