3ec329fcb1da63c9e6eaa1640bffceabf3622037
LuLuCheeky <lulucheekypro@gmail.com>
2025-04-06 09:01:03 +0000

    Apr 6, 2025, 9:01 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -89,4 +89,5 @@
 # 1. Modify your code to remove the comma (",") that appears
 #    either at the start or end of your sentence
 order = ""
-input("Give me the order pls")
+said = input("Give me the order pls")
+order = order 
