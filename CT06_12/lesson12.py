95bee10ca5f4fe0a5577c4f42f482eac86e76b57
LuLuCheeky <lulucheekypro@gmail.com>
2025-04-06 09:09:52 +0000

    Apr 6, 2025, 9:09 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -90,4 +90,7 @@
 #    either at the start or end of your sentence
 order = ""
 while True:
-    user = input
+    user = input("What would you like to order? ")
+    if user == "end":
+        break
+    
