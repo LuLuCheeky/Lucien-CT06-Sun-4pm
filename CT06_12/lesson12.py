46b98614114d39bbacc09afedc41a0b3ad297d40
LuLuCheeky <lulucheekypro@gmail.com>
2025-04-06 08:41:21 +0000

    Apr 6, 2025, 8:41 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -69,6 +69,6 @@ while True:
         print("There are 30 visitors.")
         break
     else:
-        print("There are" , visitors + ".")
+        print("There are" , str(visitors) + ".")
         visitors = visitors + 1
         
