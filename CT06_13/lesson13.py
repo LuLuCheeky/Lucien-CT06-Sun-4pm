e733b8ab4ee863cde7fcfd1d9eda76d11f20661e
LuLuCheeky <lulucheekypro@gmail.com>
2025-04-13 08:06:41 +0000

    Apr 13, 2025, 8:06 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -2,4 +2,5 @@ import random
 while True:
     var1 = random.randint(1, 10)
     var2 = random.randint(1, 10)
-    guess = int(input("What is" , var1 , "+" , var))
+    guess = int(input("What is" , var1 , "+" , var2 + "?"))
+    
