4791aef14f76092dd264226c8cbd5236b640a6ea
LuLuCheeky <lulucheekypro@gmail.com>
2025-04-13 08:57:34 +0000

    Apr 13, 2025, 8:57 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,5 +1,5 @@
 groceries = ["Apples", "Bread", "Carrots", "Dates", "Eggs", "Flour", "Grapes", "Herbs"]
 groceries[7] = "Herbs"
 groceries.append["Ice"]
-groceries.insert[2]
+groceries.insert[2],"Bread"
 print(groceries)
