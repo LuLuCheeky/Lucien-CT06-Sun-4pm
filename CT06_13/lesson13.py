9df2db3256364e140224a19b4ee19bf3d00f1fd2
LuLuCheeky <lulucheekypro@gmail.com>
2025-04-13 08:54:44 +0000

    Apr 13, 2025, 8:54 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,4 +1,3 @@
-groceries = [
-    "Apples", "Bread", "Carrots", "Dates", "Eggs", "Flour", "Grapes", "Herbs"]
+groceries = ["Apples", "Bread", "Carrots", "Dates", "Eggs", "Flour", "Grapes", "Herbs"]
 groceries[7] = "Herbs"
 print(groceries)
