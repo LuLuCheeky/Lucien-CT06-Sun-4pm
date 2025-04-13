1d627f7423d8ccd481f528658f849b6efe3d23ad
LuLuCheeky <lulucheekypro@gmail.com>
2025-04-13 09:23:02 +0000

    Apr 13, 2025, 9:23 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -3,7 +3,14 @@ groceries[7] = "Herbs"
 groceries.append("Ice")
 groceries.insert(1,"Bananas")
 del(groceries[2])
-print(groceries)
+
+# Task 2: List of groceries (part 2)
+# 1. Use a 'for' loop and print out all the groceries on your list
+# 2. If grocery == "Apples", print "<grocery name>: I need 5 of these"
+# 3. If grocery == "Carrots", print "<grocery name>: I need 3 of
+#    these"
+# 4. If name == "Grapes", print "<grocery name>: Get the FarmFresh
+#    brand"
 for grocery in groceries:
     print(grocery)
     if grocery == "Apples":
