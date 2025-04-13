95393f6e573aa2b488966d65f68c4fad09f15012
LuLuCheeky <lulucheekypro@gmail.com>
2025-04-13 09:09:51 +0000

    Apr 13, 2025, 9:09 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,6 +1,7 @@
-groceries = ["Apples", "Bread", "Carrots", "Dates", "Eggs", "Flour", "Grapes", "Herbs"]
-groceries[7] = "Herbs"
-groceries.append("Ice")
-groceries.insert(1,"Bananas")
-del(groceries[2])
-print(groceries)
+# groceries = ["Apples", "Bread", "Carrots", "Dates", "Eggs", "Flour", "Grapes", "Herbs"]
+# groceries[7] = "Herbs"
+# groceries.append("Ice")
+# groceries.insert(1,"Bananas")
+# del(groceries[2])
+# print(groceries)
+
