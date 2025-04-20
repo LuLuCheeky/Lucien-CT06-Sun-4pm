a7e8b41d6e9d15685e4fe53767e4067ff7fe585c
LuLuCheeky <lulucheekypro@gmail.com>
2025-04-20 08:56:43 +0000

    Apr 20, 2025, 8:56 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -2,9 +2,10 @@ import turtle
 
 window = turtle.Screen()
 window.setup(width=600, height=400)
-window.mainloop
 
 t = turtle.Turtle()
 t.shape("turtle")
 t.fillcolor("orange")
 t.speed(10)
+
+window.mainloop
