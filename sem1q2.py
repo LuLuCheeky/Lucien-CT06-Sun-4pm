a29f9792a49635ba9c38a1ca3cde41a2e2053c10
LuLuCheeky <lulucheekypro@gmail.com>
2025-05-04 08:24:26 +0000

    May 4, 2025, 8:24 AM

--- sem1q2.py
+++ sem1q2.py
@@ -1,37 +1,25 @@
 
-#Task 1
-# Print numbers from 10 to 200 using the while loop
-# Your numbers must be in multiples of 10.
-# 10 must be first number printed, and 200 the last number.
+# Basic List Functions
+# Do the following operations to the list provided below
+# Write the code below each question.
 
-# Example: 10, 20, 30 ..... 180, 190, 200.
-# Note that the numbers do not need to be printed in one line.
-# Write your code here
+planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
 
-
+# 1. Write code below to print the 3rd item 
+#    in this list using index e.g. earth
 
 
-
-###################################################
-
-# Task 2
-# Code a password checker to protect your code!
-
-# Assign a password "superpass123" to a variable
-# Ask the user to enter a password
-# If the password matches, print “Access Granted”
-# If the password does not match, print “Access Denied”
-
-# Write your code here
+# 2. Write code to append neptune to this list.
 
 
+# 3. Elon Musk has conquered Mars. 
+#    Rename Mars in the list to be "muskworld"
 
 
+# 4. Remove uranus from this list.
 
-##########################################
-num1 = 10
-while True:
-    print(num1)
-    num1 = num1 + 10
-    if num1 == 210:
-        break
+
+# 5. Using a for loop, print all the planets 
+#    from this list one by one.
+
+print(plane)
