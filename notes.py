eb426b2e2aee2707099962034ba6643d25f2139c
LuLuCheeky <lulucheekypro@gmail.com>
2025-05-11 05:21:30 +0000

    May 11, 2025, 5:21 AM

--- notes.py
+++ notes.py
@@ -125,4 +125,5 @@ count = 0
 while count < 5:
     print("Hello World!")
     count += 1
-#This will print "Hello World!" uncil t
+#This will print "Hello World!" uncil the variable count is equal to 5
+
