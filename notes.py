001cbdc03f42303d6ecda8c6ad87eeeea26daed7
LuLuCheeky <lulucheekypro@gmail.com>
2025-05-11 05:28:34 +0000

    May 11, 2025, 5:28 AM

--- notes.py
+++ notes.py
@@ -188,5 +188,10 @@ else:
     print("The number is not 5")
 #This will print "The number is not 5" since num is not 5
 #Elifs
-#Elifs can be used to check if a condition is true when the if condition is false and sometimes when the else condion
+#Elifs can be used to check if a condition is true when the if condition is false
+#Example of an elif statement
+num = 10
+if num == 5:
+    print("The number is 5")
+    
 
