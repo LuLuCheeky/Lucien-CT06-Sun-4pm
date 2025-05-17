ab4160379969255e465e454f100b5110a7c1e614
LuLuCheeky <lulucheekypro@gmail.com>
2025-05-17 07:00:24 +0000

    May 17, 2025, 7:00 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -9,17 +9,4 @@
 # print(counter)  
 
 def isEven(n):
-    if n % 2 == 0:
-        print("True")
-    else:
-        print("False")
-
-num = [
-    3,
-    8,
-    23456,
-    1243
-]
-
-for i in range(len(num)):
-    isEven(num[i + 1])
+    if n
