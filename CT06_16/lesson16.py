9e61e5bad0d1ff20ca95d68a88c7de1c0b06a664
LuLuCheeky <lulucheekypro@gmail.com>
2025-05-18 08:34:14 +0000

    May 18, 2025, 8:34 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -1,1 +1,7 @@
-print("Hello from lesson 16")
+import turtle 
+
+def setup_screen(screenWidth, screenHeight):
+    """Set up the turtle screen with a specific width and height."""
+    turtle.setup(screenWidth, screenHeight)
+    turtle.bgcolor("lightblue")
+    turtle.title("Turtle Graphics")
