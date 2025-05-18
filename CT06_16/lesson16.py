4aead391f70effa3baff76c7b6787a4e7429fc08
LuLuCheeky <lulucheekypro@gmail.com>
2025-05-18 08:46:59 +0000

    May 18, 2025, 8:46 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -6,6 +6,8 @@ def setup_screen(screenWidth, screenHeight):
     window.setup(width=screenWidth, height=screenHeight)
     return window
 
+
+
 setupW = 300
 setupH = 500
 window = setup_screen(setupW, setupH)
